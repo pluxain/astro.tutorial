bff7265533bb6ac48c3e0777b2a7b9497f365486
Pascal Luxain <pascal.luxain@gmail.com>
2024-09-09 10:23:37 +0200

    (refactor) use CollectionEntry to define types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,18 +1,1 @@
 export type URL = string;
-
-export type MarkdownPostFrontmatter = {
-  author: string;
-  description: string;
-  pubDate: Date;
-  title: string;
-  image?: {
-    url: URL;
-    text: string;
-  };
-  tags?: Array<string>;
-};
-
-export type Post = {
-  url: URL;
-  frontmatter: MarkdownPostFrontmatter;
-};
